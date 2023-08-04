405b35a425f383371f1f65580a0d96d71c2129f4
ross-p-smith <rosmith@microsoft.com>
2023-08-04 11:14:43 +0000

    Modularise the Terraform

--- infrastructure/outputs.tf
+++ infrastructure/outputs.tf
@@ -1,15 +1,16 @@
 output "dev_apim" {
   description = "Deployed resource groups names."
   value = {
-    resource_group_name = azurerm_resource_group.dev_rg.name
-    apim_name           = azurerm_api_management.dev_apim.name
+    resource_group_name = module.apim_dev.apim.resource_group_name
+    apim_name           = module.apim_dev.apim.apim_name
   }
 }
 
 output "prod_apim" {
   description = "Deployed resource groups names."
   value = {
-    resource_group_name = azurerm_resource_group.prod_rg.name
-    apim_name           = azurerm_api_management.prod_apim.name
+    resource_group_name = module.apim_prod.apim.resource_group_name
+    apim_name           = module.apim_prod.apim.apim_name
+
   }
 }
