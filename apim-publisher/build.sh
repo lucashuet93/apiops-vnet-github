23e0aa06525c8c1e795597f5af01dfc78773ecc1
Andrii Aheiev <andriia@backbase.com>
2023-08-28 14:20:07 +0200

    apim publisher Dockerfile: cleanup wget. Publisher test pod: add apim-access-secret

--- apim-publisher/build.sh
+++ apim-publisher/build.sh
@@ -5,7 +5,7 @@ APIM_IMAGE_REPO=gcacrapim.azurecr.io/apim-publisher
 version_file_path="$(dirname -- ${BASH_SOURCE[0]})/APIM_VERSION"
 APIM_VERSION=$(cat $version_file_path)
 VERSION=$(echo $APIM_VERSION | sed -e 's/^v//')
-VERSION="${VERSION}-1"
+VERSION="${VERSION}-5"
 
 docker build --platform linux/amd64 \
   --build-arg APIM_VERSION=$APIM_VERSION \
@@ -16,3 +16,7 @@ az acr login --name gcacrapim
 docker push $APIM_IMAGE_REPO:$VERSION
 docker tag $APIM_IMAGE_REPO:$VERSION $APIM_IMAGE_REPO:latest
 docker push $APIM_IMAGE_REPO:latest
+
+# example
+# docker build --platform linux/amd64 --build-arg APIM_VERSION=v4.9.0 -t gcacrapim.azurecr.io/apim-publisher:4.9.0-4 .
+# docker push gcacrapim.azurecr.io/apim-publisher:4.9.0-4
