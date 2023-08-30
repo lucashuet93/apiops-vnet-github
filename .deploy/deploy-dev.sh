b1f37fc94eda1dce91d58fb784c5ab63a3c0448e
Andrii Aheiev <andriia@backbase.com>
2023-08-30 11:26:47 +0200

    play with backend deploy

--- .deploy/deploy-dev.sh
+++ .deploy/deploy-dev.sh
@@ -1,7 +1,18 @@
 #!/usr/bin/env bash
 
-helm -n apim-be-dev upgrade --install apim-demo-app oci://gcacrapim.azurecr.io/charts/apim-demo-app \
-  -f dev.yaml \
-  --set-file apim.policy=apim/policy.xml \
-  --version 0.0.1-38 \
+VERSION=0.0.6-52
+#VERSION=2.0.1-47
+#VERSION=2.0.1-47
+#VERSION=2.0.1-47
+
+MAJOR_VERSION=${VERSION%.*.*}
+
+ENV_NAME=dev
+
+helm -n apim-be-${ENV_NAME} upgrade --install apim-demo-app oci://gcacrapim.azurecr.io/charts/apim-demo-app \
+  -f ${ENV_NAME}.yaml \
+  --set-file apim.apiInformation=../apimartifacts/apis/apim-be-${ENV_NAME}/apiInformation.json \
+  --set-file apim.openApiSpec=../.apispecs/apim-demo-app.v${MAJOR_VERSION}.yaml \
+  --set-file apim.policy=../apimartifacts/apis/apim-be-dev/policy.xml \
+  --version ${VERSION} \
   --wait
